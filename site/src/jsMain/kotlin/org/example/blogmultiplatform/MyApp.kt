6eb0a015eb7907c78d2fddc8c550d6c14757edbb
funyin <funyin.kash@gmail.com>
2023-10-20 15:12:36 +0100

    updated font family

--- site/src/jsMain/kotlin/org/example/blogmultiplatform/MyApp.kt
+++ site/src/jsMain/kotlin/org/example/blogmultiplatform/MyApp.kt
@@ -1,16 +1,20 @@
 package org.example.blogmultiplatform
 
 import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.fontFamily
 import com.varabyte.kobweb.compose.ui.modifiers.minHeight
 import com.varabyte.kobweb.core.App
 import com.varabyte.kobweb.core.init.InitKobweb
 import com.varabyte.kobweb.core.init.InitKobwebContext
 import com.varabyte.kobweb.silk.SilkApp
 import com.varabyte.kobweb.silk.components.layout.Surface
+import com.varabyte.kobweb.silk.components.style.ComponentStyle
 import com.varabyte.kobweb.silk.components.style.common.SmoothColorStyle
 import com.varabyte.kobweb.silk.components.style.toModifier
 import com.varabyte.kobweb.silk.init.InitSilk
 import com.varabyte.kobweb.silk.init.InitSilkContext
+import org.example.blogmultiplatform.res.Res
 import org.jetbrains.compose.web.css.vh
 
 @InitSilk
@@ -26,11 +30,21 @@ fun initKobweb(ctx: InitKobwebContext) {
     }
 }
 
+val baseStyle by ComponentStyle {
+    cssRule(" .silk-span-text, p") {
+        Modifier.fontFamily(Res.Strings.FONT_FAMILY)
+    }
+}
+
 @App
 @Composable
 fun MyApp(content: @Composable () -> Unit) {
     SilkApp {
-        Surface(SmoothColorStyle.toModifier().minHeight(100.vh)) {
+        Surface(
+            SmoothColorStyle.toModifier()
+                .minHeight(100.vh)
+                .then(baseStyle.toModifier())
+        ) {
             content()
         }
     }
